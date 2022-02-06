fda7c348c25eb72f9704273ef909eb427a8c8d63
ResulAktoz <97792215+ResulAktoz@users.noreply.github.com>
2022-02-06 12:08:01 +0300

    Add files via upload

---
+++ KodBloklarındakiLokalDeğişkenler/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(int a = 0;a<10;a++) {
+			System.out.println(a);
+			
+		}
+		//yukarýdaki a'yý burda çaðýrýp yazdýramam eðer fordan önce main içinde tanýmlarsak burada çýktý olarak alabiliriz.
+
+	}
+
+}
