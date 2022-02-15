d5388c1db3e7351b62264d8858c34e1518ba17ac
ResulAktoz <97792215+ResulAktoz@users.noreply.github.com>
2022-02-15 12:38:50 +0300

    Add files via upload

---
+++ oopGiris/src/oopGiris/Araba.java
@@ -0,0 +1,10 @@
+package oopGiris;
+
+	public class Araba {
+	public String renk;
+	public int tekerlekler;
+	public String motor;
+	public String model;
+	
+
+}
---
+++ oopGiris/src/oopGiris/Test.java
@@ -0,0 +1,15 @@
+package oopGiris;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Araba araba1 = new Araba();
+		araba1.renk = "Kýrmýzý";
+		araba1.tekerlekler = 4;
+		araba1.motor= "1.6";
+		araba1.model= "Opel";
+		System.out.println(araba1.motor);
+		
+	}
+
+}
