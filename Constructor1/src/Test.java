82d5a02251ea4b9dec4c5f7bfb919448f23f482e
ResulAktoz <97792215+ResulAktoz@users.noreply.github.com>
2022-02-17 10:30:21 +0300

    Add files via upload

---
+++ Constructor1/src/Test.java
@@ -0,0 +1,25 @@
+
+public class Test {
+	public static void main(String[] args) {
+		
+		/*Account account2 =  new Account();
+		//System.out.println(account2);
+		
+		Account account1= new Account("123456", 1000.0, "Resul", "sdadad", "325");
+		
+		
+		System.out.println(+account1.getBakiye());
+		account1.paraÇekme(700.0);*/
+		
+		Account account1 =  new Account();
+		Account account2 =  new Account("Resul Aktoz","adasd@gmail","234");
+		/*System.out.println(account2.getEmail());
+		System.out.println(account2.getBakiye());*/
+		account2.bilgileriGöster();
+		
+		
+		
+		
+	}
+
+}
