ab273aba315fc74eea0f41f96174b17c74701e60
ResulAktoz <97792215+ResulAktoz@users.noreply.github.com>
2022-02-28 12:27:37 +0300

    Add files via upload

---
+++ 15 String Sınıfı/src/StringSınıfı/Main.java
@@ -0,0 +1,70 @@
+package StringSýnýfý;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		String a= "Resul";
+//		String b= new String("Ertuðrul");
+		
+		//System.out.println(b);
+//		System.out.println("Harf sayýsý" +b.length());
+//		
+//		System.out.println("0. indeks: " +b.charAt(0));
+//		
+//		System.out.println("2. indeks: " +b.charAt(2));
+//		System.out.println("Son eleman: " +b.charAt(b.length()-1));
+		
+//		for (int i=0; i<b.length(); i++) {
+//			//System.out.println(b.charAt(i));
+//			
+//			
+//				
+//			}
+		/*System.out.println(b.startsWith("Er")); //er ile baþlýyorsa true dönüyor.
+		System.out.println(b.endsWith("yul"));*/
+		
+		//System.out.println(b.indexOf('r'));
+		/*System.out.println(b.lastIndexOf('u')); //son gördüðünü gösteriyor
+		System.out.println(b.toLowerCase());// hepsini küçüðe çeviriyor.
+		System.out.println(b.toUpperCase()); // herpsini büyük harfe çeviriyor. */
+		
+		
+//		String a= "1923";
+//		int b= Integer.parseInt(a);
+//		System.out.println(b);
+		
+		/*int b1= 1923;
+		String a1= String.valueOf(b1);
+		System.out.println(a1);*/
+		
+//		String a= "Resul";
+//		String b= "Resul";
+//		if(a==b) {
+//			System.out.println("Eþitler");
+//		}else {
+//			System.out.println("Eþit deðiller");
+//		}
+		
+		String b1= new String("Resul");
+		String b2= new String("Resul");
+		/*if(b1==b2) {
+			System.out.println("Eþitler");
+		}else {
+			System.out.println("Eþit deðiller");
+		}*/
+		if(b1.equals(b2)) {
+			System.out.println("Eþitler");
+		}else {
+			System.out.println("Eþit deðiller");
+		}
+		
+		
+		
+		}
+	
+	
+
+	}
+
+
